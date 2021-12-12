e14f1738691a2f3f88e5447c62101497eb7e4d7f
wex335 <wex335games@yandex.ru>
2021-12-12 12:21:29 +0300

    fix bugs? add stickers

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from PIL import Image
-res = (1000,2000)
+res = (5000,3000)
 bg=Image.open('back.png')
 bg_w, bg_h = bg.size
 background = Image.new('RGB', res)
 w, h = background.size
 for i in range(0, w, bg_w):
     for j in range(0, h, bg_h):
-        bg = Image.eval(bg, lambda x: x+(i+j)/1000)
+        #bg = Image.eval(bg, lambda x: x+(i+j)/1000)
         background.paste(bg, (i, j))
+print('bg inited')
 import random
-for i in range(1000):
-    pim = Image.open(f'stickers/{random.randrange(1,33)}.png').convert("RGBA")
-    grad = random.randrange(20,120)
-    p = pim.rotate(random.randrange(-15,15)).resize((int(pim.width*grad/100),int(pim.height*grad/100)))
+for i in range(3000):
+    pim = Image.open(f'stickers/{random.randrange(1,61)}.png').convert("RGBA")
+    grad = random.randrange(90,110)
+    p = pim.rotate(random.randrange(-10,10)).resize((int(pim.width*grad/100),int(pim.height*grad/100)))
     px = random.randrange(int(-p.width/2),res[0])
     py = random.randrange(int(-p.height/2),res[1])
     background.paste(p,(px, py),p)
-background.show()
+    print(i)
+#background.show()
 background.save('img.png')
